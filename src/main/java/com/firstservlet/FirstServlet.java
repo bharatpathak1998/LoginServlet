f7a3b2b3445bf243e510b3a03f887458289d723f
bharatpathak1998 <bharatpathak099@gmail.com>
2023-02-03 16:21:54 +0530

    Added : Functionality To Create My First Servlet

---
+++ src/main/java/com/firstservlet/FirstServlet.java
@@ -0,0 +1,19 @@
+package com.firstservlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World Bharat My First Servlet!</h3>");
+        out.close();
+    }
+}
